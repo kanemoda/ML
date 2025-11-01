0156f2fb737ae87120a6d7b198e3893ecbaf21fa
kanemoda <efedenizbaglar45@gmail.com>
2025-11-01 15:06:32 +0300

    Extract, Transform, Load (ETL)

---
+++ include/data.hpp
@@ -0,0 +1,34 @@
+#ifndef __DATA_H__
+#define __DATA_H__
+
+#include <vector>
+#include <stdint.h>
+#include <stdio.h>
+
+class data
+{
+    std::vector<uint8_t> * feature_vector; //No Class at end.
+    uint8_t label;
+    int enum_label; // A -->1 , B --> 2
+
+    public:
+    data();
+    ~data();
+    void set_feature_vector(std::vector<uint8_t> *);
+    void append_to_feature_vector(uint8_t);
+    void set_label(uint8_t);
+    void set_enumerated_label(int);
+
+    //Get methods
+    int get_feature_vector_size();
+    uint8_t get_label();
+    uint8_t get_enumerated_label();
+    std::vector<uint8_t> * get_feature_vector();
+};
+
+
+
+
+
+
+#endif // __DATA_H__
